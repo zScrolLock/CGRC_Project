fdec3f6ddad14b99a2683d76be033c010f577fdf
Luis Augusto <luis.augustocm@hotmail.com>
2021-12-08 17:48:53 -0300

    Commit full project

---
+++ project/Project/Assets/Scripts/Controllers/ArmaController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmaController : MonoBehaviour
+{
+    public ParticleSystem armaRaios;
+    public float damage;
+    public Camera cam;
+    Transform inimigo;
+    RaycastHit hit;
+    public Healthbar energyBar;
+
+    bool usando;
+    public float energyBarVelocity = 40;
+
+    public AudioClip somAtirar;
+    AudioSource audioSr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSr = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (usando)
+        {
+            energyBar.TakeDamage(energyBarVelocity * Time.deltaTime);
+
+            if (!audioSr.isPlaying)
+            {
+                audioSr.PlayOneShot(somAtirar);
+            }
+            
+        }
+        if (Input.GetButton("Fire1") && energyBar.health > 0)
+        {
+            
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+            {
+                if (hit.transform.CompareTag("Inimigo"))
+                {
+                    usando = true;
+                    hit.transform.GetComponent<EnemyController>().TomarDano();
+                    inimigo = hit.transform;
+                    if (armaRaios.isStopped)
+                    {
+                        armaRaios.Play();
+                    }
+                }
+            }
+
+            
+        }
+        if(hit.transform == null)
+        {
+            audioSr.Stop();
+            usando = false;
+            armaRaios.Stop();
+            if(inimigo != null)
+            {
+                inimigo.GetComponent<EnemyController>().PararDeTomarDano();
+            }
+            
+        }
+        else if (Input.GetButtonUp("Fire1") || hit.transform != inimigo)
+        {
+            audioSr.Stop();
+            usando = false;
+            armaRaios.Stop();
+            inimigo.GetComponent<EnemyController>().PararDeTomarDano();
+        }
+        else if(energyBar.health <= 0)
+        {
+            audioSr.Stop();
+            usando = false;
+            armaRaios.Stop();
+            inimigo.GetComponent<EnemyController>().PararDeTomarDano();
+        }
+
+        
+    }
+}
---
+++ project/Project/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    bool objetoNaMao;
+    public float lookRadius = 10f;
+    GameObject[] itensArremessaveis;
+    public Transform target;
+    NavMeshAgent agent;
+    bool EstaArremessando;
+    int randomNumber;
+    Animator animator;
+    float distance;
+
+    public bool TomandoDano;
+
+    public Transform objetoSpawn;
+
+    public float forca;
+
+    int damping = 2;
+
+    public Healthbar enemyLife;
+
+    public float Damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objetoNaMao = false;
+        EstaArremessando = false;
+        itensArremessaveis = GameObject.FindGameObjectsWithTag("Arremessaveis");
+        //target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TomandoDano)
+        {
+            enemyLife.TakeDamage(Damage*Time.deltaTime);
+        }
+        if(enemyLife.health <= 0)
+        {
+            animator.SetBool("Estunado", true);
+            agent.isStopped = true;
+        }
+
+        PegarItem();
+        if(agent.velocity.x > 0.1f || agent.velocity.x < -0.1f || agent.velocity.z > 0.1f || agent.velocity.z < -0.1f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if(itensArremessaveis[randomNumber] != null) 
+        {
+            distance = Vector3.Distance(itensArremessaveis[randomNumber].transform.position, transform.position);
+
+            if(distance <= 2.3f && !objetoNaMao && !TomandoDano)
+            {
+                objetoNaMao = true;
+                agent.isStopped = true;
+                
+
+                animator.SetTrigger("isThrowing");
+            }
+        }
+
+        if (objetoNaMao)
+        {
+            Vector3 dir = target.position - itensArremessaveis[randomNumber].transform.position;
+
+
+            Quaternion rotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+        }
+
+
+
+    }
+
+    void PegarItem()
+    {
+        
+        if (!EstaArremessando)
+        {
+            randomNumber = Random.Range(0, itensArremessaveis.Length);
+        }
+        if (itensArremessaveis[randomNumber] != null )
+        {
+            
+            EstaArremessando = true;
+            if(distance > 2.5f && !objetoNaMao)
+            {
+                agent.SetDestination(itensArremessaveis[randomNumber].transform.position);
+            }
+            
+        }
+    }
+
+    public void ArremessarItem()
+    {
+        
+        itensArremessaveis[randomNumber].transform.SetParent(null,true);
+        itensArremessaveis[randomNumber].GetComponent<Rigidbody>().isKinematic = false;
+        Vector3 dir = target.position - itensArremessaveis[randomNumber].transform.position;
+        dir = dir.normalized;
+        itensArremessaveis[randomNumber].GetComponent<Rigidbody>().AddForce(dir * forca);
+
+        StartCoroutine(Esperar());
+
+    }
+    public void LargarItem()
+    {
+        itensArremessaveis[randomNumber].transform.SetParent(null, true);
+        itensArremessaveis[randomNumber].GetComponent<Rigidbody>().isKinematic = false;
+        objetoNaMao = false;
+    }
+
+    IEnumerator Esperar()
+    {
+        yield return new WaitForSeconds(4f);
+
+        objetoNaMao = false;
+        EstaArremessando = false;
+        
+        agent.isStopped = false;
+        
+
+
+    }
+
+    public void SegurarItem()
+    {
+        itensArremessaveis[randomNumber].transform.position = objetoSpawn.transform.position;
+        itensArremessaveis[randomNumber].transform.parent = objetoSpawn;
+        
+    }
+
+    public void TomarDano()
+    {
+        TomandoDano = true;
+        if (objetoNaMao)
+        {
+            LargarItem();
+        }
+        EstaArremessando = false;
+        agent.isStopped = true;
+        animator.SetBool("BeingHit", true);
+        
+
+    }
+
+    public void PararDeTomarDano()
+    {
+        TomandoDano = false;
+        animator.SetBool("BeingHit", false);
+        agent.isStopped = false;
+    }
+}
